1dd151b949f3d60c3079763b1f0a5a9cf152de2e
Junior <jvieiradr1008@gmail.com>
2024-10-21 16:02:45 -0300

    erro

--- controllers/listarclientes.js
+++ controllers/listarclientes.js
@@ -1,12 +1,13 @@
-import banco from '../db.js';
+//import banco from '../db.js';
 
 const listarClientes = (req, res) => {
-    const query = 'SELECT * FROM barbearia.clientes ORDER BY nome;'
+/*    const query = 'SELECT * FROM barbearia.clientes ORDER BY nome;'
 
     banco.query(query, (erro, data) => {
         if(erro) return res.status(400).json(erro);
         return res.status(200).json(data);
-    });
+    });*/
+    return res.json('Passou aqui');
 };
 
 export default listarClientes;
