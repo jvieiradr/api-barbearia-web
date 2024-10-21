47731af5401cf5099d03caf9bf92d1ef425a2cc4
Junior <jvieiradr1008@gmail.com>
2024-10-21 16:49:24 -0300

    corrigo

--- controllers/listarclientes.js
+++ controllers/listarclientes.js
@@ -1,8 +1,8 @@
 import banco from '../db.js';
 
 const listarClientes = (req, res) => {
-    const query = 'SELECT * FROM barbearia.clientes ORDER BY nome;'
-/*
+  /*  const query = 'SELECT * FROM barbearia.clientes ORDER BY nome;'
+
     banco.query(query, (erro, data) => {
         if(erro) return res.json('passei erros');
         return res.json('passei dados');*
