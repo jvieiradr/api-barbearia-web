01b06010f81df2f1833fc497ea00d6d81281a13b
Junior <jvieiradr1008@gmail.com>
2024-10-21 12:12:48 -0300

    primeiro

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import cors from 'cors';
+
+const api = express();
+api.use(express.json());
+api.use(cors());
+
+
+const port = process.env.PORT || 8800;
+
+api.listen(8080, () => {
+    console.log('Servidor Iniciado...');
+});
