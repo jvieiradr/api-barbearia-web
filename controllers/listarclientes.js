597a419b970b3a28ceae761bfcf5ac88271ebbcf
José Vieira Costa Júnior <jvieiradr1008@gmail.com>
2024-10-21 17:41:40 -0300

    Update listarclientes.js

--- controllers/listarclientes.js
+++ controllers/listarclientes.js
@@ -1,7 +1,4 @@
-// import banco from '../db.js';
-
 const listarClientes = (req, res) => {
-    const query = 'SELECT * FROM barbearia.clientes ORDER BY nome;'
     return res.json('Passei aqui')
 };
 
