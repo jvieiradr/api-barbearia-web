dd98507280c2339595d27726473d7189e78f6727
Junior <jvieiradr1008@gmail.com>
2024-10-21 16:23:49 -0300

    corrigo

--- controllers/listarclientes.js
+++ controllers/listarclientes.js
@@ -4,8 +4,8 @@ const listarClientes = (req, res) => {
     const query = 'SELECT * FROM barbearia.clientes ORDER BY nome;'
 
     banco.query(query, (erro, data) => {
-        if(erro) return res.status(400).json(erro);
-        return res.status(200).json(data[0].nome);
+        if(erro) return res.status(400).json('passei erros');
+        return res.status(200).json('passei dados');
     });
 };
 
