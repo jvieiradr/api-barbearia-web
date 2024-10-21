fd8dc6babc1eec199ea7b70e0d98d06a43dcc74a
Junior <jvieiradr1008@gmail.com>
2024-10-21 16:00:35 -0300

    listar

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 
+import listarClientes from './controllers/listarclientes.js';
+
 const api = express();
 api.use(express.json());
 api.use(cors());
-
 
 const port = process.env.PORT || 8800;
 
 api.listen(8080, () => {
     console.log('Servidor Iniciado...');
 });
+
+api.get('/listarclientes', listarClientes);
 
 api.get("/", (req, res) => {
     return res.json('Deu Certo!');
