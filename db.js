ea8fe125358eed67cfbd1787aa17f6c6939751aa
Junior <jvieiradr1008@gmail.com>
2024-10-23 17:53:33 -0300

    mysql2

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
-import mysql from 'mysql';
+import mysql from 'mysql2';
 
 const banco = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'password',
+    port: 3306,
     database: 'barbearia'
 });
 
