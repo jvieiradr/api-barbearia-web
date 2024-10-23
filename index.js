37295e5c504391336f7b4c39ecdecb0f7031b907
Junior <jvieiradr1008@gmail.com>
2024-10-23 14:57:18 -0300

    render

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 
-import listarClientes from './controllers/listarclientes.js';
+import listarClientes  from './controllers/listarclientes.js';
 
 const api = express();
 api.use(express.json());
-
-const corsConfig = {
-    origin: '',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-}
-app.use(cors(corsConfig))
-app.options("", cors(corsConfig))
+api.use(cors());
 
 const port = process.env.PORT || 8800;
 
-api.listen(8800, () => {
+api.listen(port, () => {
     console.log('Servidor Iniciado...');
 });
 
