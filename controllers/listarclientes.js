3ab2c1a9ae69b31584f0c08afb63409ee434fd7a
Junior <jvieiradr1008@gmail.com>
2024-10-23 15:17:37 -0300

    mysql

--- controllers/listarclientes.js
+++ controllers/listarclientes.js
@@ -1,7 +1,12 @@
 import banco from '../db.js';
 
 const listarClientes = (req, res) => {
-    return res.json('Passei aqui')
+    const query = 'SELECT * FROM barbearia.clientes ORDER BY nome;'
+
+    banco.query(query, (erro, data) => {
+        if(erro) return res.status(400).json(erro);
+        return res.status(200).json(data);
+    });
 };
 
 export default listarClientes;
